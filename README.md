5d71c5ead8247fdaaa1d92741cdc7f888fba05ee
Karina <kbrilhante@gmail.com>
2024-09-03 23:00:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 04-Loops-EX-Inicio
+ 
---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define ex00
+
+/*** EXERCICIOS LOOPS ***/
+
+#ifdef ex01
+/*
+	DESAFIO: escreva um programa que calcula o fatorial de um número
+	4! = 4 x 3 x 2 x 1
+	8! = 8 x 7 x 6 x 5 x 4 x 3 x 2 x 1
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
+
+#ifdef ex02
+/*
+	DESAFIO: Escreva um programa que recebe o número inicial, a razão e o
+	comprimento e exiba a Progressão Aritimética
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
+
+#ifdef ex03
+/*
+	DESAFIO: Mesma coisa, porém Progressão Geométrica
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0;
+}
+#endif
